5fb20270bf8bbdd3c04db9a64ee7e2079b9accbd
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-04-19 18:36:40 -0600

    added framework for app

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,12 +1,36 @@
 import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Login from './Login';
+import Logout from './Logout';
+import Profile from './Profile';
+import MyHomes from './MyHomes';
+import Houses from './Houses';
 
-function Nav() {
-  const [selected, setSelected] = useState([
-    'home',
-    'profile',
-    'login',
-    'logout',
-  ]);
+export default function Nav() {
+  const [selected, setSelected] = useState('login');
+
+  const renderPage = () => {
+    if (selected === 'Login') {
+      return <Login />;
+    }
+    if (selected === 'Logout') {
+      return <Logout />;
+    }
+    if (selected === 'Profile') {
+      return <Profile />;
+    }
+    if (selected === 'MyHomes') {
+      return <MyHomes />;
+    }
+  };
+
+  const handleNavChange = (page) => setSelected(page);
+
+  return (
+    <div>
+      <NavTabs selected={selected} handleNavChange={handleNavChange} />
+      <Houses />
+      {renderPage()}
+    </div>
+  );
 }
-
-export default Nav;
